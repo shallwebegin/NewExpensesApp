b49035ccd6e0b6c91dca36f2d381088cfa534650
Berke Kartal <shallwebegin@users.noreply.github.com>
2023-12-17 21:06:38 +0300

    added
    added this files

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:expenselist/widgets/expenses.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
+}
---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,36 @@
+import 'package:expenselist/widgets/expenses_list/expenses_list.dart';
+import 'package:expenselist/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 29.99,
+        date: DateTime.now(),
+        category: Category.leisure)
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('The chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
+    );
+  }
+}
